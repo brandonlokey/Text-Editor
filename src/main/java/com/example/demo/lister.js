5c9f2005162d0dd1ec95369f0f795a9c3859c22b
Brandon Lokey <brandon.lokey96@gmail.com>
2020-04-20 13:16:08 -0400

    Added lister js file.

---
+++ src/main/java/com/example/demo/lister.js
@@ -0,0 +1,21 @@
+var filenames = fetch("http://localhost:8080/getFiles");
+
+console.log(filenames);
+
+function makeList() {
+    var list = document.createElement('ul');
+
+    for(var i = 0; i < filenames.length; i++) {
+        // Create the list item:
+        var item = document.createElement('li');
+
+        // Set its contents:
+        item.appendChild(document.createTextNode(array[i]));
+
+        // Add it to the list:
+        list.appendChild(item);
+    }
+
+    // Finally, return the constructed list:
+    return list;
+}
