a70dd2ee3b983f6b0a9d915d0761c6c05acad0f2
Kevin Levitz <tug97195@temple.edu>
2020-04-19 22:12:20 -0400

    added file write / append functions

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,42 @@
 package com.example.demo;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Date;
+import java.util.Calendar;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+    	//write_new("ASDFFFF");
+    	//append_file("1234.txt", "fsdasdadasdsadsad");
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    
+    
+    //working
+    public static void write_new(String body) throws IOException {
+    	String fileName = "1234.txt"; //Needs to be more dynamic this is just to test
+    	BufferedWriter w = new BufferedWriter(new FileWriter(fileName));
+        w.write(body);
+        w.close();
+    }
+    
+    //working
+    public static void append_file(String fileName, String body) throws IOException {
+    	
+    	String str = "Hello World";
+        BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true));
+        writer.append(' ');
+        writer.append(str);
+        writer.close();
+    	
+    }
+    
 }
--- src/main/java/com/example/demo/file.java
+++ src/main/java/com/example/demo/file.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
 public class file {
-	public int id;
+
+	public String fileName;
 	public String title;
 	public String body;
 	
-	public file(int id, String title, String body) {
-		this.id = id;
+	public file(String fileName, String title, String body) {
+		this.fileName = fileName;
 		this.title = title;
 		this.body = body;
 		
