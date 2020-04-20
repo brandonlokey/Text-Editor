072baa836e8fbe8d2be589e490c92bf9b7c41f7f
Kevin Levitz <tug97195@temple.edu>
2020-04-19 22:36:45 -0400

    added request for saving a file

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,9 +6,14 @@ import java.io.IOException;
 import java.sql.Date;
 import java.util.Calendar;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -17,15 +22,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
 
     public static void main(String[] args) throws IOException {
-    	//write_new("ASDFFFF");
+    	//write_file("ASDFFFF");
     	//append_file("1234.txt", "fsdasdadasdsadsad");
         SpringApplication.run(DemoApplication.class, args);
     }
     
     //working
-    public static void write_new(String body) throws IOException {
-    	String fileName = "1234.txt"; //Needs to be more dynamic this is just to test
-    	BufferedWriter w = new BufferedWriter(new FileWriter(fileName));
+    public static void write_file(String id, String body) throws IOException {
+    	BufferedWriter w = new BufferedWriter(new FileWriter(id));
         w.write(body);
         w.close();
     }
@@ -33,10 +37,9 @@ public class DemoApplication {
     //working
     public static void append_file(String fileName, String body) throws IOException {
     	
-    	String str = "Hello World";
         BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true));
         writer.append(' ');
-        writer.append(str);
+        writer.append(body);
         writer.close();
     	
     }
@@ -46,5 +49,18 @@ public class DemoApplication {
         System.out.println("This is a test");
     }
     
+    
+    @RequestMapping("/writeFile")
+    @CrossOrigin(maxAge = 3600)
+    @ResponseBody
+    public void putUser(HttpServletResponse response, @RequestParam("id") String id,
+                        @RequestParam("body") String body) throws IOException {
+        response.getWriter().println("SUCCESS");
+        String fileName = id + ".txt";
+        write_file(fileName, body);
+        System.out.println("File saved");
+        //Example URL http://localhost:8080/writeFile?id=2345&body=abc
+        
+    }
     
 }
