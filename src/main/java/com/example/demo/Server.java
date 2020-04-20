33c7bca62bf624801d2730d326d4e3c0277cc9f7
Brandon Lokey <57154350+gourmetpez@users.noreply.github.com>
2020-04-20 17:54:44 -0400

    JS function submits json of file names, working on unordered list

--- src/main/java/com/example/demo/Server.java
+++ src/main/java/com/example/demo/Server.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 import java.io.*;
+import java.util.List;
 import java.util.Scanner;
 
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.MediaType;
@@ -14,86 +16,84 @@ import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
+@CrossOrigin(origins = "*")
 public class Server {
 
     public static void main(String[] args) throws IOException {
-    	//write_file("ASDFFFF");
-    	//append_file("1234.txt", "fsdasdadasdsadsad");
+        //write_file("ASDFFFF");
+        //append_file("1234.txt", "fsdasdadasdsadsad");
         SpringApplication.run(Server.class, args);
     }
-    
+
     //working
     public static void write_file(String id, String body) throws IOException {
-    	BufferedWriter w = new BufferedWriter(new FileWriter(id));
+        BufferedWriter w = new BufferedWriter(new FileWriter(id));
         w.write(body);
         w.close();
     }
-    
+
     //working
     public static void append_file(String fileName, String body) throws IOException {
-    	
+
         BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true));
         writer.append(' ');
         writer.append(body);
         writer.close();
-    	
+
     }
-    
+
     public static String load_file(String id) throws FileNotFoundException {
-    	File curFile = new File(id);
-    	String data = "";
-    	try {
-			Scanner fr = new Scanner(curFile);
-			while(fr.hasNextLine()) {
-				return data = data + fr.nextLine();
-			}
+        File curFile = new File(id);
+        String data = "";
+        try {
+            Scanner fr = new Scanner(curFile);
+            while (fr.hasNextLine()) {
+                return data = data + fr.nextLine();
+            }
 
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			data = "There was an error";
-		}
-		return data;
-    	
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            data = "There was an error";
+        }
+        return data;
+
     }
 
-    @RequestMapping("/greeting")
+    @RequestMapping("/test")
     public void testFunc() {
         System.out.println("This is a test");
     }
-    
+
     @RequestMapping("/writeFile")
     @CrossOrigin(maxAge = 3600)
     @ResponseBody
     public void writeFile(HttpServletResponse response, @RequestParam("id") String id,
-                        @RequestParam("body") String body) throws IOException {
+                          @RequestParam("body") String body) throws IOException {
         response.getWriter().println("SUCCESS");
         String fileName = id + ".txt";
         write_file(fileName, body);
         System.out.println("File saved");
         //Example URL http://localhost:8080/writeFile?id=2345&body=abc
-        
+
     }
-    
+
     @RequestMapping("/loadFile")
     @CrossOrigin(maxAge = 3600)
     @ResponseBody
     public void loadFile(HttpServletResponse response, @RequestParam("id") String id) throws IOException {
-       
+
         String fileName = id + ".txt";
         String data = load_file(fileName);
         response.getWriter().println(data);
         //Example URL http://localhost:8080/loadFile?id=1234
-        
+
     }
 
-    /* Returns a json of all text files in currently in directory
-        Currently retrieves names of text files correctly, not returning JSON yet
-     */
-    @RequestMapping(path = "/getFiles", method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    // Returns array of all .txt files in current directory
+    @GetMapping(path = "/getFiles", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public static JsonArray fileReturn() {
+    public static String[] fileReturn() {
         String[] pathnames;
         File f = new File(System.getProperty("user.dir"));
         FilenameFilter filter = new FilenameFilter() {
@@ -108,11 +108,6 @@ public class Server {
         for (String pathname : pathnames) {
             System.out.println(pathname);
         }
-
-        JsonArray json_array= new JsonArray();
-        for (int i = 0; i < pathnames.length; i++) {
-            json_array.add(pathnames[i]);
-        }
-        return json_array;
+        return pathnames;
     }
 }
--- src/main/java/com/example/demo/lister.js
+++ src/main/java/com/example/demo/lister.js
@@ -1,8 +1,29 @@
-var filenames = fetch("http://localhost:8080/getFiles");
+// Returns JSON of file names and lists them in unordered list *unordered list not functional*
 
-console.log(filenames);
+async function getFileList(notes) {
+    const resp = await fetch("http://127.0.0.1:8080/getFiles");
+    notes = await resp.json();
+    console.log(notes);
 
-function makeList() {
+    var list = document.createElement('ul');
+
+    listContainer = document.createElement('div');
+    listElement = document.createElement('ul');
+    numItems = notes.length;
+
+    document.getElementsByTagName('body')[0].appendChild(listContainer);
+    listContainer.appendChild(listElement);
+
+    for (var i = 0; i <numItems; ++i) {
+        listItem = document.createElement('li');
+
+        listItem.innerHTML = notes[i];
+    }
+}
+
+getFileList();
+
+/*async function makeList() {
     var list = document.createElement('ul');
 
     for(var i = 0; i < filenames.length; i++) {
@@ -18,4 +39,4 @@ function makeList() {
 
     // Finally, return the constructed list:
     return list;
-}
+}*/
