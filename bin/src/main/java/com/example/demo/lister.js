29f34e1627921eab8ff44f6f805eb55d9490009c
Kevin Levitz <tug97195@temple.edu>
2020-04-22 00:01:49 -0400

    Resolved fetch issue

--- bin/src/main/java/com/example/demo/lister.js
+++ bin/src/main/java/com/example/demo/lister.js
@@ -8,26 +8,28 @@ async function getFileList(notes) {
     var list = document.createElement('ul');
     numItems = notes.length;
 
-    notes.forEach(function (note)
+    notes.forEach(async function (note)
     {
         var li = document.createElement('li');
         var id = note.substring(0, note.length-4);
-        var body = getbody(note);
-        li.innerHTML = "<a href='index.html?&filename=" + id + "&body=" + body + "'>" + note + "</a>";
-        list.appendChild(li);
+        //var body = getbody(note);
+		console.log("This is your ID = " + id);
+		fetch("http://127.0.0.1:8080/getBody?id=" + id)
+		  .then(function(response) {
+			return response.json();
+		  }).then(function(data) {
+			//console.log(data); 
+			li.innerHTML = "<a href='index.html?&filename=" + id + "&body=" + data.body + "'>" + note + "</a>";
+			
+			
+			list.appendChild(li);
+		  });		
     })
     var container = document.querySelector('#list');
     container.appendChild(list);
 }
 
 getFileList();
-
-async function getbody(filename) {
-    const currfile = filename.substring(0, filename.length - 4);
-    const resp = await fetch("http://127.0.0.1:8080/getBody?id=" + currfile);
-    const body = await resp.json();
-    return body;
-}
 
 /*async function makeList() {
     var list = document.createElement('ul');
